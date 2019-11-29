d073411ad8ff4464816b33f75ce65841b809cc04
Roelf <rdiederi@student.wethinkcode.co.za>
2019-11-29 18:31:54 +0200

    added ex03

--- 01_dockerfiles/ex03/run.sh
+++ 01_dockerfiles/ex03/run.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-docker build -t ex03 .
-docker run -it --rm -p 8080:80 -p 8022:22 -p 8443:443 --privileged ex03
+docker build -t ex03 . && docker run -it --rm -p 8080:80 -p 8022:22 -p 8443:443 --privileged ex03
